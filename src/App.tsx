c1e85f012db20fe1eb0c110df63acdad7cbbb19f
Mahdi MirMohamadi <mahdimirmohamadi13@gmail.com>
2023-08-15 03:25:40 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Typography from "@mui/material/Typography";
+
+function App() {
+  return (
+    <>
+      <Typography color={"red"}>آقا سلام</Typography>
+    </>
+  );
+}
+
+export default App;
